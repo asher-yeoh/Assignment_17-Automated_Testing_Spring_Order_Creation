62d478b5bf1889bd609e9e56de5ec981ee06731e
ASHER <asher.yeoh@dell.com>
2019-08-17 21:24:15 +0800

    completed v1.0

--- spring/src/main/java/learning/appointmentapp/repositories/LineItemRepository.java
+++ spring/src/main/java/learning/appointmentapp/repositories/LineItemRepository.java
@@ -1,11 +1,8 @@
 package learning.appointmentapp.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import learning.appointmentapp.entities.LineItem;
-import learning.appointmentapp.entities.Order;
 
 /**
  * LineItemRepository
